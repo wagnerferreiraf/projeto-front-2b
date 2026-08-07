ef7d4079c3c1a5c51f758fe063c73c9102c74077
wagnerferreiraf <professorwagnerferreira@gmail.com>
2026-08-07 17:40:53 -0400

    Aula do dia 07/08/2026

--- src/app/contato/page.js
+++ src/app/contato/page.js
@@ -1,11 +1,22 @@
+'use client';
+import { useState } from 'react';
 import styles from './page.module.css';
 
 export default function Contato() {
     let msg = 'Entre em contato:';
-    let cont = 0;
+    // let cont = 0; // forma antiga
+    const [cont, setCont] = useState(0);
+    const [texto, setTexto] = useState('');
+    // declare uma constante booleana com o nome de mostrar, com valor inicial true
+    // faça o que está em cima, só que coloque o nome "mostrar"
+    const [mostrar, setMostrar] = useState(true);
 
     const somar = () => {
-        cont++;
+        // cont++;
+        // setCont(cont + 1);
+        setCont(cont + 1);
+        // setCont(prev => prev + 1);
+        // setCont(prev => prev + 1);
         console.log('Contador: ' + cont);
     }
 
@@ -14,7 +25,17 @@ export default function Contato() {
             <h1>Contato</h1>
             <p>{msg}</p>
             <p>Contador: {cont}</p>
-            <button onClick={somar}>Somar</button>
+            <button onClick={() => somar()}>Somar</button>
+            <button>{mostrar ? 'Esconder' : 'Mostrar'}</button> {/*Exiba o texto "Esconder" se a variável mostrar for verdadeira, e exiba o texto "Mostrar" se a variável for falsa*/}
+            {mostrar && <form>
+                <fieldset>
+                    <p>{texto ? texto : 'Digite algum conteúdo'}</p>
+                    <label htmlFor='msg'>Digite aqui: </label>
+                    <input type="text" id='msg' name='msg' onKeyUp={(e) => setTexto(e.target.value)} />
+                    <button type='button' onClick={() => alert(texto)}>Clique aqui</button>
+                    {/* ao clicar no botão, dispare um alerta (alert) com o conteúdo digitado no input */}
+                </fieldset>
+            </form>}
         </main>
     )
 }
